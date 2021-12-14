4dfa910a527efabe87d7e7a3d555984ae2de52ed
Matthew Tang <matthew.tang@connect.hku.hk>
2021-12-15 00:55:21 +0800

    add selectcharacter logic

--- src/Components/SelectCharacter/index.js
+++ src/Components/SelectCharacter/index.js
@@ -1,13 +1,115 @@
 import React, { useEffect, useState } from "react";
 import "./SelectCharacter.css";
+import { ethers } from "ethers";
+import { CONTRACT_ADDRESS, transformCharacterData } from "../../constants";
+import myEpicGame from "../../utils/MyEpicGame.json";
 
 /*
  * Don't worry about setCharacterNFT just yet, we will talk about it soon!
  */
 const SelectCharacter = ({ setCharacterNFT }) => {
+  const [characters, setCharacters] = useState([]);
+  const [gameContract, setGameContract] = useState(null);
+
+  useEffect(() => {
+    const { ethereum } = window;
+
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const gameContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        myEpicGame.abi,
+        signer
+      );
+      setGameContract(gameContract);
+    } else {
+      console.log("No ethereum found");
+    }
+  }, []);
+
+  useEffect(() => {
+    const getCharacters = async () => {
+      try {
+        console.log("Getting characters");
+
+        const characterTx = await gameContract.getAllDefaultCharacters();
+        console.log("charactersTx: ", characterTx);
+
+        const characters = characterTx.map((characterData) =>
+          transformCharacterData(characterData)
+        );
+
+        setCharacters(characters);
+      } catch (error) {
+        console.log("some error when getting characters:", error);
+      }
+    };
+
+    const onCharacterMint = async (sender, tokenId, characterIndex) => {
+      console.log(
+        `characterNFTMinted - sender: ${sender}, tokenId: ${tokenId}, characterIndex: ${characterIndex}`
+      );
+
+      if (gameContract) {
+        alert(
+          `Your NFT is all done -- see it here: https://testnets.opensea.io/assets/${gameContract}/${tokenId.toNumber()}`
+        );
+        const characterNFT = await gameContract.checkIfUserHasNFT();
+        console.log("characterNFT: ", characterNFT);
+        setCharacterNFT(transformCharacterData(characterNFT));
+      }
+    };
+
+    if (gameContract) {
+      getCharacters();
+
+      gameContract.on("CharacterNFTMinted", onCharacterMint);
+    }
+
+    return () => {
+      if (gameContract) {
+        gameContract.off("CharacterNFTMinted", onCharacterMint);
+      }
+    };
+  }, [gameContract]);
+
+  const renderCharacters = () =>
+    characters.map((character, index) => (
+      <div className="character-item" key={character.name}>
+        <div>
+          <p>{character.name}</p>
+        </div>
+        <img src={character.imageURI} alt={character.name} />
+        <button
+          type="button"
+          className="character-mint-button"
+          onClick={mintCharacterNFTAction(index)}
+        >
+          {`Mint ${character.name}`}
+        </button>
+      </div>
+    ));
+
+  const mintCharacterNFTAction = (characterIndex) => async () => {
+    try {
+      if (gameContract) {
+        console.log("minting character");
+        const mintTx = await gameContract.mintCharacterNFT(characterIndex);
+        await mintTx.wait();
+        console.log("mintTx:", mintTx);
+      }
+    } catch (error) {
+      console.log("some error when minting character:", error);
+    }
+  };
+
   return (
     <div className="select-character-container">
       <h2>Mint Your Hero. Choose wisely.</h2>
+      {characters.length > 0 && (
+        <div className="character-grid">{renderCharacters()}</div>
+      )}
     </div>
   );
 };
