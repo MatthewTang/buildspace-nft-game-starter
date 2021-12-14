14a2474d85930b0cb7cb1d8a58763c2730b618e8
Matthew Tang <matthew.tang@connect.hku.hk>
2021-12-15 00:19:57 +0800

    add abi, contract addr, call epicgame.userhasnftcharacter func

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useEffect } from "react";
 import twitterLogo from "./assets/twitter-logo.svg";
 import "./App.css";
+import SelectCharacter from "./Components/SelectCharacter";
+import { CONTRACT_ADDRESS, transformCharacterData } from "./constants";
+import myEpicGame from "./utils/MyEpicGame.json";
+import { ethers } from "ethers";
 
 // Constants
 const TWITTER_HANDLE = "_buildspace";
@@ -8,6 +12,17 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
   const [currAccount, setCurrAccount] = React.useState(null);
+  const [characterNFT, setCharacterNFT] = React.useState(null);
+
+  const checkNetwork = async () => {
+    try {
+      if (window.ethereum.networkVersion !== "4") {
+        alert("please connect to the Rinkeby test network");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const checkIfWalletIsConntected = async () => {
     try {
@@ -36,6 +51,56 @@ const App = () => {
   useEffect(() => {
     checkIfWalletIsConntected();
   }, []);
+
+  useEffect(() => {
+    const fetchNFTMetadata = async () => {
+      console.log("checking for character nft on addr", currAccount);
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const gameContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        myEpicGame.abi,
+        signer
+      );
+
+      console.log("gameContract", gameContract);
+
+      const tx = await gameContract.checkIfUserHasNFT();
+      if (tx.name) {
+        console.log("user has character nft");
+        setCharacterNFT(transformCharacterData(tx));
+      } else {
+        console.log("user does not have character nft");
+      }
+    };
+
+    if (currAccount) {
+      console.log("currAccount", currAccount);
+      fetchNFTMetadata();
+    }
+  }, [currAccount]);
+
+  const renderContent = () => {
+    if (!currAccount) {
+      return (
+        <div className="connect-wallet-container">
+          <img
+            src="http://49.media.tumblr.com/87092d1a1fac85c04fa6560405df3595/tumblr_o3bubgokIY1v6l2a0o1_1280.gif"
+            alt="Monty Python Gif"
+          />
+          <button
+            className="connect-wallet-button cta-button"
+            onClick={connectWalletAction}
+          >
+            Connect Wallet to get Started
+          </button>
+        </div>
+      );
+    } else if (currAccount && !characterNFT) {
+      return <SelectCharacter setCharacterNFT={setCharacterNFT} />;
+    }
+  };
 
   const connectWalletAction = async () => {
     try {
@@ -69,20 +134,9 @@ const App = () => {
             running forward, no matter what... Don't you ever stop running and
             don't you ever look behind you... All that exists is what's ahead.
           </p>
-          <div className="connect-wallet-container">
-            <img
-              src="http://49.media.tumblr.com/87092d1a1fac85c04fa6560405df3595/tumblr_o3bubgokIY1v6l2a0o1_1280.gif"
-              alt="Monty Python Gif"
-            />
-            <button
-              className="connect-wallet-button cta-button"
-              onClick={connectWalletAction}
-            >
-              Connect Wallet to get Started
-            </button>
-          </div>
         </div>
         <div className="footer-container">
+          {renderContent()}
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
             className="footer-text"
---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = "0xde5012A143e22FEd4EaDbAAA846448717A40e4be";
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
